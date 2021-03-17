6cd88422b84c7361aced7eead7332b2ca71eae1d
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-03-17 17:53:54 -0300

    Add API call function

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const input = document.getElementById("input");
 const button = document.getElementById("button");
+const display = document.getElementById("display");
+const loading = document.getElementById("loading");
+
+const ul = document.createElement("ul");
+const title = document.createElement("li");
+const runTime = document.createElement("li");
 
 function getValue() {
-  const input = document.getElementById("input");
-
   if (input.value === "") console.log("You haven't entered anything.");
   else {
+    loading.style.visibility = "visible";
+
     fetch("https://www.omdbapi.com/?t=" + input.value + "&apikey=d54c9527")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.Title);
-        console.log(data);
+        if (data.Title === undefined)
+          console.log("We couldn't find the entered move :c");
+        else {
+          title.innerHTML = data.Title;
+          runTime.innerHTML = data.Runtime;
+
+          ul.appendChild(title);
+          ul.appendChild(runTime);
+          display.appendChild(ul);
+        }
+        loading.style.visibility = "hidden";
       });
   }
 }
