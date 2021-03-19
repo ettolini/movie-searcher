f1447bc98390dce2e9775d91c244fac198af4482
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-03-19 15:58:46 -0300

    Disable button when event is being handled.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const input = document.getElementById("input");
-const button = document.getElementById("button");
+const button = document.getElementById("searchButton");
 const display = document.getElementById("display");
 const loading = document.getElementById("loading");
-
-const ul = document.createElement("ul");
-const title = document.createElement("li");
-const runTime = document.createElement("li");
 
 function handleClick() {
   if (input.value === "") console.log("You haven't entered anything.");
   else {
+    const ul = document.createElement("ul");
+    const title = document.createElement("li");
+    const runTime = document.createElement("li");
+    const plot = document.createElement("li");
+
+    button.disabled = true;
     loading.style.visibility = "visible";
 
     fetch("https://www.omdbapi.com/?t=" + input.value + "&apikey=d54c9527")
@@ -20,12 +22,15 @@ function handleClick() {
         else {
           title.innerHTML = data.Title;
           runTime.innerHTML = data.Runtime;
+          plot.innerHTML = data.Plot;
 
           ul.appendChild(title);
           ul.appendChild(runTime);
+          ul.appendChild(plot);
           display.appendChild(ul);
         }
         loading.style.visibility = "hidden";
+        button.disabled = false;
       });
   }
 }
