c35e6e36ce98c05da9380d3718bf9022ec290a5e
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-03-19 19:22:58 -0300

    Don't allow searching the same input in a row.
    If the user tries to submit an input that's already being displayed,
    a message will be printed on console.
    Deleted an image that wasn't used.

--- index.js
+++ index.js
@@ -11,39 +11,31 @@ const plot = document.createElement("li");
 function handleClick() {
   if (input.value === "") console.log("You haven't entered anything.");
   else {
-    // TODO: hacer esto bien
-    // if (display.hasChildNodes()) {
-    //   console.log(display.hasChildNodes());
-    //   const list = display.childNodes;
-    //   console.log(list[0].childNodes[0].innerHTML);
+    if (input.value === title.innerHTML)
+      console.log("That movie's info is already being displayed.");
+    else {
+      button.disabled = true;
+      loading.style.visibility = "visible";
 
-    //   if (list[1].innerHTML === input.value) {
-    //     console.log("That movie's info is already being displayed.");
-    //     return;
-    //   }
-    // }
+      fetch("https://www.omdbapi.com/?t=" + input.value + "&apikey=d54c9527")
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.Title === undefined)
+            console.log("We couldn't find the entered movie :c");
+          else {
+            title.innerHTML = data.Title;
+            runTime.innerHTML = data.Runtime;
+            plot.innerHTML = data.Plot;
 
-    button.disabled = true;
-    loading.style.visibility = "visible";
-
-    fetch("https://www.omdbapi.com/?t=" + input.value + "&apikey=d54c9527")
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.Title === undefined)
-          console.log("We couldn't find the entered movie :c");
-        else {
-          title.innerHTML = data.Title;
-          runTime.innerHTML = data.Runtime;
-          plot.innerHTML = data.Plot;
-
-          ul.appendChild(title);
-          ul.appendChild(runTime);
-          ul.appendChild(plot);
-          display.appendChild(ul);
-        }
-        loading.style.visibility = "hidden";
-        button.disabled = false;
-      });
+            ul.appendChild(title);
+            ul.appendChild(runTime);
+            ul.appendChild(plot);
+            display.appendChild(ul);
+          }
+          loading.style.visibility = "hidden";
+          button.disabled = false;
+        });
+    }
   }
 }
 
