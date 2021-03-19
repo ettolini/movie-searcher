8585a542be139ff0bba2a7c1253e11ae3e81c33c
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-03-19 18:17:22 -0300

    Add styles

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const button = document.getElementById("searchButton");
 const display = document.getElementById("display");
 const loading = document.getElementById("loading");
 
+const ul = document.createElement("ul");
+const title = document.createElement("li");
+const runTime = document.createElement("li");
+const plot = document.createElement("li");
+
 function handleClick() {
   if (input.value === "") console.log("You haven't entered anything.");
   else {
-    const ul = document.createElement("ul");
-    const title = document.createElement("li");
-    const runTime = document.createElement("li");
-    const plot = document.createElement("li");
+    // TODO: hacer esto bien
+    // if (display.hasChildNodes()) {
+    //   console.log(display.hasChildNodes());
+    //   const list = display.childNodes;
+    //   console.log(list[0].childNodes[0].innerHTML);
+
+    //   if (list[1].innerHTML === input.value) {
+    //     console.log("That movie's info is already being displayed.");
+    //     return;
+    //   }
+    // }
 
     button.disabled = true;
     loading.style.visibility = "visible";
@@ -18,7 +30,7 @@ function handleClick() {
       .then((response) => response.json())
       .then((data) => {
         if (data.Title === undefined)
-          console.log("We couldn't find the entered move :c");
+          console.log("We couldn't find the entered movie :c");
         else {
           title.innerHTML = data.Title;
           runTime.innerHTML = data.Runtime;
